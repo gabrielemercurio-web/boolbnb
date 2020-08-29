97657bea66365350d3c1ff9c9b442e2f3eadcb09
Marco De Clara <marcodeclara@gmail.com>
2020-08-29 15:53:32 +0200

    adds users and messages tables, and user and message models

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->char('first_name');
+            $table->char('last_name');
+            $table->date('date_of_birth');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
