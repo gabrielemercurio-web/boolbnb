460fb06d59a27e11f3752ec7b8bb6964e982eadd
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-09-03 22:56:42 +0200

    Add Search box + Details single house

--- resources/views/guest/houses/show.blade.php
+++ resources/views/guest/houses/show.blade.php
@@ -3,15 +3,35 @@
 @section('content')
 
     <div class="container">
-        <div class="row">
-
-            <div class="input-group col-6 offset-3 d-flex justify-content-center">
-                <input class="form-control py-2 border-right-0 border" type="search" value="search" id="example-search-input">
-                <span class="input-group-append">
-                    <div class="input-group-text bg-transparent"><i class="fa fa-search"></i></div>
-                </span>
+        <div class="row row-1">
+            <div class="col-6 offset-3">
+                <div class="search-house input-group">
+                    <input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <div class="input-group-append">
+                        <button class="btn" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
+                    </div>
+                </div>
             </div>
+        </div>
 
+        <div class="row row-2">
+            <div class="image-house col-6">
+                <img src="{{ asset('img/homepage.jpg') }}" alt="">
+            </div>
+            <div class="text-house col-6">
+                <h1>Apartment Title</h1>
+                <small>Place, Address</small>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias debitis optio fuga eveniet aut quia omnis officiis est minima commodi non, dolorem consectetur incidunt, quo dolores magni qui illum aliquid?</p>
+                <div class="sevices d-flex flex-column flex-wrap">
+                    <span>Rooms</span>
+                    <span>Beds</span>
+                    <span>Bathrooms</span>
+                    <span>m2</span>
+                    <span>Wifi</span>
+                    <span>Car Park</span>
+                    <span>Reception</span>
+                </div>
+            </div>
         </div>
     </div>
 
