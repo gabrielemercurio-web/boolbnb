30b0664c76df46434a62af62896a2fc1232f2c90
attraverso <attraverso.dev@gmail.com>
2020-09-05 15:29:11 +0200

    hides tomtom api key

--- resources/js/tomtom.js
+++ resources/js/tomtom.js
@@ -1,16 +1,15 @@
-// let long = Number("{{$house->longitude}}");
-// let lat = Number("{{$house->latitude}}");
-// let myCoordinates = [long, lat];
+/* * Show map in house details page * */
+let long = $('#my-maps').attr('data-longitude');
+let lat = $('#my-maps').attr('data-latitude');
 
-let myCoordinates = [9.13568, 45.46163];
+let myCoordinates = [long, lat];
 
-// function callTomTom() {
-	let map = tt.map({
-		container: 'map',
-		key: process.env.MIX_TOMTOM_API_KEY, //fill with own key
-		style: 'tomtom://vector/1/basic-main',
-		center: myCoordinates,
-		zoom: 15
-	});
-	let marker = new tt.Marker().setLngLat(myCoordinates).addTo(map);
-// }
+let map = tt.map({
+	container: 'map',
+	key: process.env.MIX_TOMTOM_API_KEY,
+	style: 'tomtom://vector/1/basic-main',
+	center: myCoordinates,
+	zoom: 15
+});
+
+let marker = new tt.Marker().setLngLat(myCoordinates).addTo(map);
--- resources/views/guest/houses/show.blade.php
+++ resources/views/guest/houses/show.blade.php
@@ -3,10 +3,11 @@
 @section('content')
 
     <div class="container">
+		{{-- SEARCHBAR --}}
         <div class="row row-1">
             <div class="col-lg-6 offset-lg-3">
                 <div class="search-house input-group">
-                    <input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" aria-describedby="button-addon2">
+                    <input type="text" class="form-control" placeholder="Search..." aria-label="Recipient's username" {{-- //XXX in che senso recipient's username? --}}aria-describedby="button-addon2">
                     <div class="input-group-append">
                         <button class="btn" type="button" id="button-addon2"><i class="fas fa-search"></i></button>
                     </div>
@@ -15,35 +16,35 @@
         </div>
 
         <div class="row row-2">
+			{{-- IMAGE SLIDER --}}
             <div id="slider" class="col-lg-6">
 
-                    <div class="images">
-                        <div class="prev">
-                            <i class="fas fa-chevron-circle-left"></i>
-                        </div>
-                        <div class="next">
-                            <i class="fas fa-chevron-circle-right"></i>
-                        </div>
+				<div class="images">
+					<div class="prev">
+						<i class="fas fa-chevron-circle-left"></i>
+					</div>
+					<div class="next">
+						<i class="fas fa-chevron-circle-right"></i>
+					</div>
 
+					{{-- <img class="visible" src="{{ $house->image_path }}" alt="img-house"> --}}
 
-                        {{-- <img class="visible" src="{{ $house->image_path }}" alt="img-house"> --}}
-
-                        <img class="visible" src="{{ asset('img/homepage.jpg') }}" alt="house">
-                        <img src="{{ asset('img/house-1.jpg') }}" alt="house-1">
-                        <img src="{{ asset('img/house-2.jpg') }}" alt="house-2">
-                        <div class="bullets">
-                            <i class="fas fa-circle visible"></i>
-                            <i class="fas fa-circle"></i>
-                            <i class="fas fa-circle"></i>
-                        </div>
-                    </div>
-
+					<img class="visible" src="{{ asset('img/homepage.jpg') }}" alt="house">
+					<img src="{{ asset('img/house-1.jpg') }}" alt="house-1">
+					<img src="{{ asset('img/house-2.jpg') }}" alt="house-2">
+					<div class="bullets">
+						<i class="fas fa-circle visible"></i>
+						<i class="fas fa-circle"></i>
+						<i class="fas fa-circle"></i>
+					</div>
+				</div>
             </div>
 
+			{{-- HOUSE INFO --}}
             <div class="text-house col-lg-6">
                 <h1>{{ $house->title }} </h1>
                 <small><i class="fas fa-map-marker-alt"></i>{{$house->address}}</small>
-            <p>{{ $house->description }}</p>
+            	<p>{{ $house->description }}</p>
                 <div class="sevices d-flex flex-column flex-wrap">
                     <span><i class="fas fa-door-open"></i>Rooms: {{ $house->nr_of_rooms }}</span>
                     <span><i class="fas fa-bed"></i>Beds: {{ $house->nr_of_beds }}</span>
@@ -57,10 +58,11 @@
         </div>
     </div>
 
+	{{-- send message & map display --}}
     <section id="map-contact">
         <div class="container">
             <div class="row row-3">
-
+				{{-- SEND MESSAGE --}}
                 <div id="contact" class="col-md-5">
                     <h1>Contact Owner</h1>
                     <form>
@@ -74,8 +76,9 @@
                             <button type="submit" class="btn">Send</button>
                         </div>
                     </form>
-                </div>
-                <div id="my-maps" class="col-md-6 offset-md-1">
+				</div>
+				{{-- MAP --}}
+				<div id="my-maps" class="col-md-6 offset-md-1" data-longitude="{{$house->longitude}}" data-latitude="{{$house->latitude}}">
                     <div id="map"></div>
                     {{-- <img src="https://www.worldeasyguides.com/wp-content/uploads/2013/01/Place-Vendome-on-Map-of-Paris.jpg" alt="Map"> --}}
                 </div>
