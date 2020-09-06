60238321a4c9788825492486573d542f861be904
Marco De Clara <marcodeclara@gmail.com>
2020-09-06 04:09:00 +0200

    Fixes minor bug

--- resources/js/tomtom.js
+++ resources/js/tomtom.js
@@ -1,15 +1,21 @@
-/* * Show map in house details page * */
-let long = $('#my-maps').attr('data-longitude');
-let lat = $('#my-maps').attr('data-latitude');
+// Get element with id = my-maps
+var map = document.getElementById('my-maps');
 
-let myCoordinates = [long, lat];
+// Find out if a map is contained in the document, run script if true
+if(document.body.contains(map)) {   
+    /* * Show map in house details page * */
+    let long = $('#my-maps').attr('data-longitude');
+    let lat = $('#my-maps').attr('data-latitude');
 
-let map = tt.map({
-	container: 'map',
-	key: process.env.MIX_TOMTOM_API_KEY,
-	style: 'tomtom://vector/1/basic-main',
-	center: myCoordinates,
-	zoom: 15
-});
+    let myCoordinates = [long, lat];
 
-let marker = new tt.Marker().setLngLat(myCoordinates).addTo(map);
+    let map = tt.map({
+        container: 'map',
+        key: process.env.MIX_TOMTOM_API_KEY,
+        style: 'tomtom://vector/1/basic-main',
+        center: myCoordinates,
+        zoom: 15
+    });
+
+    let marker = new tt.Marker().setLngLat(myCoordinates).addTo(map);
+}
