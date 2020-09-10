f5f927e856dd362a3633bbb0c9b8e3becf117e37
Giovanni Scotti <scottigiovanni93@gmail.com>
2020-09-10 17:59:35 +0200

    fix view upr homepage

--- resources/views/upr/houses/homepage.blade.php
+++ resources/views/upr/houses/homepage.blade.php
@@ -3,34 +3,37 @@
 
 @section('content')
 	<div class="home-wrapper">
-        <div class="container">
-            <div class="home">
-                <div class="overlay"></div>
-                <div class="col-lg-6">
-                    <form action="{{ route('upr.houses.homepage') }}" method="GET" class="search-house input-group">
-                        <input type="text" class="form-control" placeholder="Search..." aria-describedby="button-addon3">
-                        <div class="input-group-append">
-                            <button class="btn" type="submit" id="button-addon3"><i class="fas fa-search"></i></button>
-                        </div>
-                    </form>
-                </div>
-            </div>
+		<div class="container">
+			{{-- JUMBO --}}
+			<div class="home">
+				<div class="overlay"></div>
+				<div class="col-lg-6">
+					<form action="{{ route('upr.houses.search') }}" method="GET" class="search-house input-group">
+						<input type="text" class="form-control" placeholder="Search..." aria-describedby="button-addon3">
+						<div class="input-group-append">
+							<button class="btn" type="submit" id="button-addon3"><i class="fas fa-search"></i></button>
+						</div>
+					</form>
+				</div>
+			</div>
+			{{-- PROMOTED HOUSES LIST --}}
             <div class="title text-center">
-                <h1>Appartamenti in Evidenza</h1>
+                <h1>Places you might enjoy</h1>
                 <hr>
             </div>
             <div class="apartment-description">
+				<div class="row">
                 @forelse ($houses as $house)
-                    <div class="apartment col-4">
+                    <div class="apartment col-lg-4 col-md-6">
                         <div class="apartment-img">
-                            <img src="{{ $house->image_path }}" class="img-fluid" alt="Responsive image">
+                            <img src="{{ $house->image_path }}" class="img-fluid" alt="image of the house">
                             <p>Sponsored</p>
                         </div>
                         <div class="description-apartment">
                             <h2>{{ $house->title }}</h2>
                             <p>{{ $house->description }}</p>
                             <a href="{{ route('upr.houses.show', ['house' => $house->id]) }}">
-                                <button type="button" class="btn btn-outline-primary btn-color">Vedi dettagli appartamento</button>
+                                <button type="button" class="btn btn-outline-primary btn-color">Access house details</button>
                             </a>
                         </div>
                     </div>
@@ -39,5 +42,6 @@
                 @endforelse
             </div>
         </div>
-    </div>
+		</div>
+	</div>
 @endsection
