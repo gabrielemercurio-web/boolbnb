2d198b2b90549c0b9880bec1696a21de6c3ee386
Gabriele Mercurio <gabrielemercurio.web@gmail.com>
2020-09-19 15:18:28 +0200

    Fixes card in index upr (my homes page)

--- resources/views/upr/houses/index.blade.php
+++ resources/views/upr/houses/index.blade.php
@@ -5,13 +5,16 @@
     <div class="container">
         <div class="row">
             <div class="col-12 my-home-title">
-                <h1>MY HOMES</h1>
+                <span>
+                    <h1>MY HOMES</h1>
+                    <a href="{{ route('upr.houses.create') }}"> <i class="fas fa-plus"></i> </a>
+                </span>
                 <hr>
             </div>
         </div>
         <div class="row">
             @foreach ($houses as $house)
-                <div class="card-upr col-md-6">
+                <div class="card-upr card-upr-index col-md-4">
                     <div>
                         <a href="{{ route('upr.houses.show', ['house' => $house->id]) }}">
                             <img src="{{ asset('storage/' . $house->image_path) }}" alt="house">
@@ -37,7 +40,7 @@
                 </div>
             @endforeach
 
-            <a href="{{ route('upr.houses.create') }}" class="create-house col-md-6">
+            <a href="{{ route('upr.houses.create') }}" class="create-house col-md-4">
                 <div> <i class="fas fa-plus"></i> </div>
             </a>
         </div>
